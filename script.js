bff801054a796f5a75677a4d0dc868acfe046c05
carolina <ci.arayagonzalez@gmail.com>
2024-04-08 18:12:28 -0400

    :sparkles: Create paddle in canvas

--- script.js
+++ script.js
@@ -7,49 +7,55 @@ canvas.height = 400
 //VARIABLES PELOTA
 const ballRadius = 10;
 //position ball
-let x = canvas.width / 2;
-let y = canvas.height - 30;
+let inicialPositionX = canvas.width / 2;
+let inicialPositionY = canvas.height - 30;
 //velocity ball
-let positionX = 2;
-let positionY = -2;
+let movePositionX = 2;
+let movePositionY = -2;
 
 //VARIALES PALETA
-const paddleWidth = 30;
+const paddleWidth = 50;
 const paddleHeight = 10;
+
+let paddlePositionX = (canvas.width - paddleWidth) / 2;
+let paddlePositionY = canvas.height - paddleHeight - 10;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.arc(inicialPositionX, inicialPositionY, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#fff";
     ctx.fill();
     ctx.closePath();
 }
-function drawPaddle() {}
+function drawPaddle() {
+    ctx.fillStyle = "#09f";
+    ctx.fillRect(paddlePositionX, paddlePositionY, paddleWidth, paddleHeight);
+}
 function drawBricks() {}
 
 function collisionDetection() {}
 function ballMovement() {
     if(
-        x + positionX > canvas.width - ballRadius ||
-        x + positionX < ballRadius
+        inicialPositionX + movePositionX > canvas.width - ballRadius ||
+        inicialPositionX + movePositionX < ballRadius
         ) 
     {
-        positionX = - positionX;
+        movePositionX = - movePositionX;
     }
 
-    if(y + positionY < ballRadius) 
+    if(inicialPositionY + movePositionY < ballRadius) 
     {
-        positionY = -positionY;
+        movePositionY = -movePositionY;
     }
 
-    if(y + positionY > canvas.height - ballRadius)
+    if(inicialPositionY + movePositionY > canvas.height - ballRadius)
     {
         console.log("game over");
         document.location.reload();
     }
     //movemos la pelota
-    x += positionX;
-    y += positionY;
+    inicialPositionX += movePositionX;
+    inicialPositionY += movePositionY;
 }
 function paddleMovement() {}
 
@@ -61,7 +67,7 @@ function draw() {
     clearCanvas();
     //Dibujando elemnetoa
     drawBall();
-    // drawPaddle();
+    drawPaddle();
     // drawBricks();
 
     //colisiones y movimientos
